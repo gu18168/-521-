eea5f9cf5591b2a2c949ba0eaba14d0e991d7858
gu18168 <gu18168@gmail.com>
2017-06-22 11:31:49 +0800

    单位换用rpx
    整体使用自适应rpx单位，Js对象优化

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,31 +1,56 @@
 var app = getApp()
-var width = 0
-var height = 0
 var QQMapWX = require('../../libs/qqmap-wx-jssdk.js')
 var qqmapsdk
+// 设备信息
+var device = {
+  width: 375, //设备可用宽度，单位px
+  height: 603, //设备可用高度，单位px
+  px2rpx: 2, //设备px转rpx值
+  rpx2px: 0.5, //设备rpx转px值
+  rwidth: 750, //设备可用宽度，单位rpx，规定750rpx
+  rheight: 1206, //设备可用高度，单位rpx
+  maprHeight: 866, //地图可用高度，单位rpx
+  mapHeight: 433 //地图可用高度，单位px
+}
+// 消息下拉单信息
+var message = {
+  top: -100, //初始-100%来完全隐藏
+  distance: 78, //本身位置与动态发布点的距离
+  minute: 5, //步行距离时间
+  disWidth: 0, //距离信息单个宽度，单位rpx
+  btnWidth: 0, //按钮单个宽度，单位rpx
+  btnMargin: 0 //按钮单个外间距，单位rpx
+}
+// Swiper控制信息
+var swiperCtrl = {
+  allEnd: 'b', //活动活跃状态，b表示活跃，w表示不活跃
+  favEnd: 'w', //动态活跃状态，b表示活跃，w表示不活跃
+  accEnd: 'w', //资料活跃状态，b表示活跃，w表示不活跃
+  current: 0 //swiper当前所在位置
+}
+// 用户基本信息
+var userBasicInfo = {
+  sex: '男', //性别
+  age: 20, //年龄
+  college: 14 //学院
+}
+// 活动基本信息
+var actInfo = {
+  actDay: 3, //活动报名剩余天数
+  actHover: '' //活动点击状态,''表示未被点击，'actHover'表示被点击
+}
+
 Page({
   data: {
     map: false,
     userInfo:{},
     long: 0,
     lat: 0,
-    top: -100,
-    height,
-    width,
-    distance: 78,
-    minute: 5,
-    disWidth: 0,
-    btnWidth: 0,
-    btnMargin: 0,
-    allEnd: 'b',
-    favEnd: 'w',
-    accEnd: 'w',
-    current: 0,
-    sex: '男',
-    age: 20,
-    college: 14,
-    actDay: 3,
-    actHover: '',
+    device,
+    message,
+    swiperCtrl,
+    userBasicInfo,
+    actInfo,
     markers: [],
     controls: [{
       id: 0,
@@ -76,31 +101,22 @@ Page({
     if (e.controlId === 2) {
       this.mapCtx.moveToLocation()
     } else if (e.controlId === 3) {
-      // wx.showModal({
-      //   title: '提示',
-      //   content: '这是一个模态弹窗',
-      //   success: function(res) {
-      //     if (res.confirm) {
-      //       console.log('用户点击确定')
-      //     } else {
-      //       console.log('用户点击却笑')
-      //     }
-      //   }
-      // })
-      if (this.data.top !== 0) {
+      if (this.data.message.top !== 0) {
         this.data.controls[0].position.top = -10
+        this.data.message.top = 0
         this.setData({
-          top: 0,
+          message: this.data.message,
           controls: this.data.controls
         })
       }
     }
   },
   tap: function() {
-    if(this.data.top === 0) {
+    if(this.data.message.top === 0) {
       this.data.controls[0].position.top = -50
+      this.data.message.top = -100
       this.setData({
-        top: -100,
+        message: this.data.message,
         controls: this.data.controls
       })
     }
@@ -133,57 +149,75 @@ Page({
   },
   swiperChange: function(e) {
     if (e.detail.current === 0) {
+      this.data.swiperCtrl.allEnd = 'b'
+      this.data.swiperCtrl.favEnd = 'w'
+      this.data.swiperCtrl.accEnd = 'w'
+      this.data.swiperCtrl.current = 0
+
       this.setData({
-        allEnd: 'b',
-        favEnd: 'w',
-        accEnd: 'w'
+        swiperCtrl: this.data.swiperCtrl
       })
     } else if (e.detail.current === 1) {
+      this.data.swiperCtrl.allEnd = 'w'
+      this.data.swiperCtrl.favEnd = 'b'
+      this.data.swiperCtrl.accEnd = 'w'
+      this.data.swiperCtrl.current = 1
+
       this.setData({
-        allEnd: 'w',
-        favEnd: 'b',
-        accEnd: 'w'
+        swiperCtrl: this.data.swiperCtrl
       })
     } else if (e.detail.current === 2) {
+      this.data.swiperCtrl.allEnd = 'w'
+      this.data.swiperCtrl.favEnd = 'w'
+      this.data.swiperCtrl.accEnd = 'b'
+      this.data.swiperCtrl.current = 2
+
+
       this.setData({
-        allEnd: 'w',
-        favEnd: 'w',
-        accEnd: 'b'
+        swiperCtrl: this.data.swiperCtrl
       })
     }
   },
   allTap: function() {
+    this.data.swiperCtrl.allEnd = 'b'
+    this.data.swiperCtrl.favEnd = 'w'
+    this.data.swiperCtrl.accEnd = 'w'
+    this.data.swiperCtrl.current = 0
+
     this.setData({
-      allEnd: 'b',
-      favEnd: 'w',
-      accEnd: 'w',
-      current: 0
+      swiperCtrl: this.data.swiperCtrl
     })
   },
   favTap: function() {
+    this.data.swiperCtrl.allEnd = 'w'
+    this.data.swiperCtrl.favEnd = 'b'
+    this.data.swiperCtrl.accEnd = 'w'
+    this.data.swiperCtrl.current = 1
+
     this.setData({
-      allEnd: 'w',
-      favEnd: 'b',
-      accEnd: 'w',
-      current: 1
+      swiperCtrl: this.data.swiperCtrl
     })
   },
   accTap: function() {
+    this.data.swiperCtrl.allEnd = 'w'
+    this.data.swiperCtrl.favEnd = 'w'
+    this.data.swiperCtrl.accEnd = 'b'
+    this.data.swiperCtrl.current = 2
+
     this.setData({
-      allEnd: 'w',
-      favEnd: 'w',
-      accEnd: 'b',
-      current: 2
+      swiperCtrl: this.data.swiperCtrl
     })
   },
   actTap: function() {
-    if (this.data.actHover === '') {
+    if (this.data.actInfo.actHover === '') {
+      this.data.actInfo.actHover = 'actHover'
       this.setData({
-        actHover: 'actHover'
+        actInfo: this.data.actInfo
       })
     } else {
+      this.data.actInfo.actHover = ''
       this.setData({
-        actHover: ''
+        actInfo: this.data.actInfo
       })
     }
   },
@@ -198,20 +232,37 @@ Page({
     //得到用户信息
     app.getUserInfo(function(userInfo){
       // console.log(userInfo)
+      if (userInfo.gender === 1) {
+        that.data.userBasicInfo.sex = '男'
+      } else {
+        that.data.userBasicInfo.sex = '女'
+      }
+
       that.setData({
-        userInfo: userInfo
+        userInfo: userInfo,
+        userBasicInfo: that.data.userBasicInfo
       })
     })
 
     //得到用户设备信息
     wx.getSystemInfo({
       success: function(res) {
+        // console.log(res);
+        that.data.device.width = res.windowWidth
+        that.data.device.height = res.windowHeight
+        that.data.device.px2rpx = 750 / res.windowWidth
+        that.data.device.rpx2px = res.windowWidth / 750
+        that.data.device.rheight = res.windowHeight * that.data.device.px2rpx
+        that.data.device.maprHeight = that.data.device.rheight - 340
+        that.data.device.mapHeight = that.data.device.height - 340 * that.data.device.rpx2px
+
+        that.data.message.disWidth = that.data.device.rwidth / 4
+        that.data.message.btnWidth = that.data.device.rwidth / 5
+        that.data.message.btnMargin = that.data.device.rwidth / 40
+
         that.setData({
-          height: res.windowHeight - 170,
-          width: res.windowWidth,
-          disWidth: res.windowWidth / 4,
-          btnWidth: res.windowWidth / 5,
-          btnMargin: res.windowWidth / 40
+          device: that.data.device,
+          message: that.data.message
         })
       }
     })
@@ -222,12 +273,10 @@ Page({
       success: function(res) {
         var latitude = res.latitude
         var longitude = res.longitude
-        that.data.controls[0].position.width = that.data.width * 1.2
-        that.data.controls[0].position.left = -(that.data.width * 0.1)
-        // that.data.controls[1].position.left = that.data.width / 2 - 10
-        // that.data.controls[1].position.top = that.data.height / 2 - 40
-        that.data.controls[1].position.top = that.data.height - 80
-        that.data.controls[2].position.top = that.data.height - 145;
+        that.data.controls[0].position.width = that.data.device.width * 1.2
+        that.data.controls[0].position.left = -(that.data.device.width * 0.1)
+        that.data.controls[1].position.top = that.data.device.mapHeight - 80
+        that.data.controls[2].position.top = that.data.device.mapHeight - 145;
         that.setData({
           long: longitude,
           lat: latitude,
@@ -260,12 +309,3 @@ Page({
     })
   }
 })
-
-// {
-//   iconPath: '/img/current.png',
-//   id: 0,
-//   latitude: 0,
-//   longitude: 0,
-//   width: 20,
-//   height: 20
-// }
