4822eeba24f299d8e39da0536338a5fbe641a4da
Nick Gu <gu18168@gmail.com>
2017-07-22 13:04:19 +0800

    config.js修改

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
-var host = '32225311.qingchun521qcloud.com';
+var host = '你的服务器域名';
 
 var config = {
   service: {
     host,
-    loginUrl: `https://${host}/login`,
-    tunnelUrl: `https://${host}/tunnel`,
-    momentUrl: `https://${host}/moment`,
-    createUrl: `https://${host}/create`,
-    getUrl: `https://${host}/get`,
-    bindUrl: `https://${host}/bind`,
-    activityUrl: `https://${host}/activity`,
-    usersUrl: `https://${host}/users`,
-    momentsUrl: `https://${host}/moments`,
-    realUrl: `https://${host}/real`,
-    berealUrl: `https://${host}/bereal`,
-    confessionUrl: `https://${host}/confession`,
-    confessionsUrl: `https://${host}/confessions`
+    loginUrl: `https://${host}/login`, //登录网址
+    tunnelUrl: `https://${host}/tunnel`, //信道网址
+    momentUrl: `https://${host}/moment`, //动态信道网址
+    createUrl: `https://${host}/create`, //创建动态网址
+    getUrl: `https://${host}/get`, //得到地图动态网址
+    bindUrl: `https://${host}/bind`, //绑定账户网址
+    activityUrl: `https://${host}/activity`, //得到活动网址
+    usersUrl: `https://${host}/users`, //得到活跃用户网址
+    momentsUrl: `https://${host}/moments`, //得到今日动态网址
+    realUrl: `https://${host}/real`, //是否真实姓名绑定网址
+    berealUrl: `https://${host}/bereal`, //绑定真实姓名网址
+    confessionUrl: `https://${host}/confession`, //创建表白网址
+    confessionsUrl: `https://${host}/confessions` //得到表白网址
   }
 };
 
